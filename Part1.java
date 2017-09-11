a03656d1946b07772e55325d52c5fbdeb9cfaeb6
limon_148 <mehadihasan.cseru@gmail.com>
2017-09-11 09:29:02 -0700

    copy the code from part1 of StringSecondAssignments

--- Part1.java
+++ Part1.java
@@ -2,32 +2,64 @@
 /**
  * Write a description of class Part1 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Mehedi Hasan)
+ * @version (11-09-2017)
  */
 public class Part1
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Part1
-     */
-    public Part1()
-    {
-        // initialise instance variables
-        x = 0;
+    public int findStopCodon(String dna, int startIndex, String stopCodon){
+        int stopIndex = dna.indexOf(stopCodon, startIndex + 3);
+        while(stopIndex != -1){
+            if((stopIndex - startIndex) % 3 == 0){
+                return stopIndex;
+            }
+            else{
+                stopIndex = dna.indexOf(stopCodon, stopIndex + 1);
+            }
+        }
+        return -1;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String findGene(String dna, int Index){
+        int startIndex = dna.indexOf("ATG", Index);
+        if(startIndex == -1){
+            return "";
+        }
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int minIndex = 0;
+        if (taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)) {
+            minIndex = tgaIndex;
+        }
+        else {
+            minIndex = taaIndex;
+        }
+        if (minIndex == -1 || (tagIndex != -1 && tagIndex < minIndex)) {
+            minIndex = tagIndex;
+        }
+        if (minIndex == -1){
+            return "";
+        }
+        return dna.substring(startIndex, minIndex + 3);
+    }
+    public void printAllGenes(String dna) {
+      int startIndex = 0;
+      while ( true ) {
+          String currentGene = findGene(dna, startIndex);
+          if (currentGene.isEmpty()) {
+              break;
+          }
+          System.out.println(currentGene);
+          startIndex = dna.indexOf(currentGene, startIndex) + currentGene.length();
+        }
+    }
+    public void testOn(String dna) {
+        System.out.println("Testing printAllGenes on " + dna);
+        printAllGenes(dna);
+    }
+    public void test() {
+        testOn("ATGATCTAATTTATGCTGCAACGGTGAAGA");
+        testOn("");
+        testOn("ATGATCATAAGAAGATAATAGAGGGCCATGTAA");
     }
 }
